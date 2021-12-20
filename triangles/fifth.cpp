e8fb0ac8c195bae33e098dbfcf851179d8e5ca33
Vlad <96255646+VladGarGat@users.noreply.github.com>
2021-12-20 14:36:19 +0600

    Create fifth.cpp

---
+++ triangles/fifth.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+int main()
+{
+    std::cout << "Enter amount of half's height of rhomb: ";
+    int num, i;
+    std::cin >> num;
+    std::cout << "Enter quantity of rhombs: ";
+    int quantity;
+    std::cin >> quantity;
+
+    for (i = 1; i <= num; ++i)
+    {
+        for (int counter = 0; counter < quantity; ++counter)
+        {
+            for (int j = 0; j < num - i; j++) std::cout << " ";
+            for (int k = 0; k < i * 2 - 1; k++) std::cout << "*";
+            for (int j = 0; j < num - i; j++) std::cout << " ";
+        }
+        std::cout << std::endl;
+    }
+    i -= 2;
+    for (; i >= 0; --i)
+    {
+        for (int counter = 0; counter < quantity; ++counter)
+        {
+            for (int j = 0; j < num - i; ++j) std::cout << " ";
+            for (int k = 0; k < i * 2 - 1; ++k) std::cout << "*";
+            for (int j = 0; j < num - i; ++j) std::cout << " ";
+        }
+        std::cout << std::endl;
+    }
+}
