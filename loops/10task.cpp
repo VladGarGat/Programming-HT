f2c2a146f86ecfde392568cbd430879909bb3ea0
Vlad <96255646+VladGarGat@users.noreply.github.com>
2021-12-24 04:26:07 +0600

    Update 10task.cpp

--- loops/10task.cpp
+++ loops/10task.cpp
@@ -1,1 +1,23 @@
-
+#include <iostream>
+int main() {
+    int n;
+    std::cout << "Enter n: ";
+    std::cin >> n;
+    int a = 1;
+    while (a <= n)
+        a *= 16;
+    std::cout << "Hexademical version of n is ";
+    while (a > 1)
+    {
+        a /= 16;
+        int d = n / a;
+        if (d > 0)
+        {
+            if (d < 10) std::cout << d;
+            else std::cout << (char)(d + 55);
+            n %= a;
+        }
+        else std::cout << "0";
+    }
+    std::cout << "\n";
+}
