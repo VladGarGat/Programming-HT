7b561e9c9d5b2b273df6ef178b21a1499f8f46c6
Vlad <96255646+VladGarGat@users.noreply.github.com>
2021-12-24 03:56:57 +0600

    Update 04task.cpp

--- loops/04task.cpp
+++ loops/04task.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include <cmath>
+int main()
+{
+    int n = 0;
+    double epsilon, sum = 0, i = 1.;
+    std::cout << "Enter epsilon: ";
+    std::cin >> epsilon;
 
+    while ((1 / ((double)n * 2. + 1)) > epsilon)
+    {
+        sum += i / ((double)n * 2. + 1);
+        i *= -1; n += 1;
+    }
+    std::cout << "pi is " << 4. * sum << "\n";
+}
