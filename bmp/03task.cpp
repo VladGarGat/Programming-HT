5d2e09e3cce7ce2ade351368a7d1691800a9bd74
Vlad <96255646+VladGarGat@users.noreply.github.com>
2021-12-26 00:57:33 +0600

    Create 03task.cpp

---
+++ bmp/03task.cpp
@@ -0,0 +1,21 @@
+#include "BmpStructures.h"
+#include "ImgFunctions.h"
+#include <iostream>
+int main()
+{
+    const char* input = "rainbow.bmp";
+    const char* output = "rainbow_grey.bmp";
+    RgbImg iimg = readRgbImg(input);
+    RgbImg outputimg = createRgbImg(iimg.height, iimg.width, { 0, 0, 0 });
+    for (size_t row = 0; row < iimg.width; row++)
+    {
+        for (size_t col = 0; col < iimg.height; col++)
+        {
+            int median = (iimg.pixels[col][row].Red + iimg.pixels[col][row].Blue + iimg.pixels[col][row].Green) / 3;
+            outputimg.pixels[col][row].Red = outputimg.pixels[col][row].Green = outputimg.pixels[col][row].Blue = median;
+        }
+    }
+    writeRgbImg(output, outputimg);
+    deleteRgbImg(iimg);
+    deleteRgbImg(outputimg);
+}
